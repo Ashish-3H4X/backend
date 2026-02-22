adc2c8d2b40088c3f2194383cb87cfe37703b365
Mr Rahul <ashish227087@gmail.com>
2026-02-23 01:32:18 +0530

    cokkies fix

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,24 +1,25 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const isAuth = async (req ,res , next) => {
+const isAuth = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
 
-try {
-    let {token} = req.cookies
-  if (!token) {
-    return res.status(500).json({message:"User does't have token "})
+    if (!token) {
+      return res.status(401).json({
+        message: "User doesn't have token",
+      });
+    }
 
-    
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = verifyToken.userId;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired token",
+    });
   }
+};
 
-  let verifyToken = await  jwt.verify(token, process.env.JWT_SECRET)
-   if (!verifyToken) {
-    return res.status(500).json({message:"User does't have valid token "})
-  }
-  req.userId = verifyToken.userId
-  next()
-  
-} catch (error) {
-   return res.status(500).json({message:`isAuth Error${error}`})
-}}
-
-export default isAuth
+export default isAuth;
