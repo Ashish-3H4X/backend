99acf4927a60dae13440cdf673e25141b82781e6
Mr Rahul <ashish227087@gmail.com>
2026-02-23 10:23:41 +0530

    frontend url setup

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:"https://frontend-me1t.vercel.app",
+  origin:"https://e-learning-frontend-liard-eight.vercel.app",
   credentials:true
 }))
 
